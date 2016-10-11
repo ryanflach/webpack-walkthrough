961be4a39f7d8abe41a2e72907f5e754c342fd1a
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-11 14:06:16 -0600

    Update webpack config and add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,15 @@ module.exports = {
   output: {
     path: __dirname,
     filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', 'css']
   }
 }
